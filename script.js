3d482e1ff67485914429e4c593237b4e1f30afb1
surduromeo <46320239+surduromeo@users.noreply.github.com>
2021-11-21 10:32:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ var popupHTML = function(photo){
   var result = "";
       result = '<strong>'+photo.title+'</strong><br>';
       result += '<a href="'+photo.url_o+'" target="_blank">';
-      result += '<img src="'+photo.url_s+'"></a>';      //was url_t; want url_s; can change to url_m if desired, but frame needs work
+      result += '<img src="'+photo.url_t+'"></a>';      //was url_t; want url_s; can change to url_m if desired, but frame needs work
       result += '<small>click pe imagine</small>';
       return result;
 	   
